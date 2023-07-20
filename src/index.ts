baf61f3fed0bf7aaece8799ca28fb95223584bb3
Todd Garrison <toddlgarrison@gmail.com>
2023-07-20 17:27:35 -0400

    Map display on screen

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,6 @@
 /// <reference types="@types/google.maps" />
 import { User } from './User';
 import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
-const user = new User();
-
-console.log(user);
-
-const company = new Company();
-
-console.log(company);
-
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-    zoom: 1,
-    center: {
-        lat: 0,
-        lng: 0
-    }
-});
+new CustomMap('map')
