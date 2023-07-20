021df99510a2f484a77a25f31484db5a7274c29b
Todd Garrison <toddlgarrison@gmail.com>
2023-07-20 16:29:14 -0400

    Add maps review docs

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,11 @@ console.log(user);
 const company = new Company();
 
 console.log(company);
+
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
